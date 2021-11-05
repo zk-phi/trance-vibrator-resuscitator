e7456903b21d1e0414144d478bd259cf8ae8250d
zk-phi <zk-phi@users.noreply.github.com>
2021-11-05 17:38:48 +0900

    update

--- WebGLChart.js
+++ WebGLChart.js
@@ -1,4 +1,15 @@
 class Chart {
+  constructor (options) {
+    this.el = document.createElement("canvas");
+    this.el.width = options.width;
+    this.el.height = options.height;
+    this.max = options.max;
+    this.min = options.min;
+    this.ctx = this.el.getContext("webgl2", { preserveDrawingBuffer: true });
+    this.ctx.getExtension("OES_standard_derivatives");
+    this.ctx.initialized = false;
+  }
+
   compileShader (kind, source) {
     const shader = this.ctx.createShader(kind);
     this.ctx.shaderSource(shader, source);
@@ -23,7 +34,7 @@ class Chart {
     return program;
   }
 
-  bindTrivialVertices (prog, attrName) {
+  useTrivialVertices () {
     const buf = this.ctx.createBuffer();
     this.ctx.bindBuffer(this.ctx.ARRAY_BUFFER, buf);
     this.ctx.bufferData(this.ctx.ARRAY_BUFFER, new Float32Array([
@@ -32,12 +43,17 @@ class Chart {
       1.0, 1.0,
       1.0, -1.0,
     ]), this.ctx.STATIC_DRAW);
+    return buf;
+  }
+
+  bindVertices (prog, attrName, vertices) {
     const loc = this.ctx.getAttribLocation(prog, attrName);
+    this.ctx.bindBuffer(this.ctx.ARRAY_BUFFER, vertices);
     this.ctx.enableVertexAttribArray(loc);
     this.ctx.vertexAttribPointer(loc, 2, this.ctx.FLOAT, false, 0, 0);
   }
 
-  makeTexture () {
+  useTexture () {
     const texture = this.ctx.createTexture();
     this.ctx.bindTexture(this.ctx.TEXTURE_2D, texture);
     this.ctx.texParameteri(this.ctx.TEXTURE_2D, this.ctx.TEXTURE_WRAP_S, this.ctx.CLAMP_TO_EDGE);
@@ -47,26 +63,15 @@ class Chart {
     return texture;
   }
 
-  constructor (options) {
-    this.el = document.createElement("canvas");
-    this.el.width = options.width;
-    this.el.height = options.height;
-    this.max = options.max;
-    this.min = options.min;
-    this.ctx = this.el.getContext("webgl2", { preserveDrawingBuffer: true });
-    this.ctx.getExtension("OES_standard_derivatives");
-    this.ctx.initialized = false;
-  }
-
   async initialize () {
     const vs = await fetch("./vertex.glsl", { cache: "no-cache" });
     const fs = await fetch("./fragment.glsl", { cache: "no-cache" });
     const prog = this.buildProgram(await vs.text(), await fs.text());
-    this.bindTrivialVertices(prog, "pos");
+    const vertices = this.useTrivialVertices();
+    this.bindVertices(prog, "pos", vertices);
     const audioLoc = this.ctx.getUniformLocation(prog, "audio");
     this.ctx.uniform1i(audioLoc, 0);
-    this.ctx.activeTexture(this.ctx.TEXTURE0);
-    this.makeTexture();
+    this.audioTexture = this.useTexture();
     const resolutionLoc = this.ctx.getUniformLocation(prog, "resolution");
     this.ctx.uniform2f(resolutionLoc, this.el.width, this.el.height)
     this.initialized = true;
@@ -77,6 +82,9 @@ class Chart {
       const min = this.min ?? Math.min(...f32Arr);
       const max = this.max ?? Math.max(...f32Arr);
       const data = f32Arr.map(v => 1 - (v - min) / (max - min));
+      /* bind audio texture */
+      this.ctx.activeTexture(this.ctx.TEXTURE0);
+      this.ctx.bindTexture(this.ctx.TEXTURE_2D, this.audioTexture);
       this.ctx.texImage2D(
         this.ctx.TEXTURE_2D, 0, this.ctx.R32F,
         data.length, 1, 0,
