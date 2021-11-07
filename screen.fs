1b9d19ab737099a8ad3248bd6e4ef98268f5b517
zk-phi <zk-phi@users.noreply.github.com>
2021-11-07 18:54:50 +0900

    bg color

--- screen.fs
+++ screen.fs
@@ -6,7 +6,7 @@ uniform vec2 resolution;
 void main (void) {
     vec2 pos = gl_FragCoord.xy / resolution;
     float v = 1. - smoothstep(value - .1, value, abs(.5 - pos.y) * 2.);
-    vec4 bg = vec4(mix(vec3(0.), vec3(.4, .2, .1), v), 1.);
+    vec4 bg = vec4(mix(vec3(0.), vec3(.30, .15, .0), v), 1.);
     vec4 fg = vec4(texture2D(texture, gl_FragCoord.xy / resolution));
     gl_FragColor = clamp(bg + fg, 0., 1.);
 }
