cc71fd7945765b3e565f6675d1e1ffc7de00bc77
zk-phi <zk-phi@users.noreply.github.com>
2021-11-07 18:39:17 +0900

    visualizer

--- buffer.fs
+++ buffer.fs
@@ -5,10 +5,13 @@ uniform vec2 resolution;
 
 void main (void) {
     vec2 pos = gl_FragCoord.xy / resolution;
-    float prev = texture2D(backbuffer, pos).a;
+    float prevVal = texture2D(backbuffer, pos).b / .25;
+
     float wave = texture2D(audio, vec2(pos.x, 0.)).r;
     float d = abs(pos.y - wave);
-    float new = clamp(pow(1. - d, 50.), 0., 1.);
-    float value = clamp(prev * .9 + new * 1., 0., 1.);
-    gl_FragColor = value * vec4(1., .5, 0., 1.);
+    float newVal = clamp(pow(1. - d, 50.), 0., 1.);
+
+    vec4 color = vec4(1., .5, .25, 1.);
+    float value = newVal * 1.5 + prevVal * .9;
+    gl_FragColor = clamp(value * color, 0., 1.);
 }
--- screen.fs
+++ screen.fs
@@ -5,8 +5,8 @@ uniform vec2 resolution;
 
 void main (void) {
     vec2 pos = gl_FragCoord.xy / resolution;
-    float v = 1. - smoothstep(0., value, abs(.5 - pos.y) * 2.);
-    vec4 bg = vec4(mix(vec3(0.), vec3(.4, .2, .0), v), 1.);
+    float v = 1. - smoothstep(value - .1, value, abs(.5 - pos.y) * 2.);
+    vec4 bg = vec4(mix(vec3(0.), vec3(.4, .2, .1), v), 1.);
     vec4 fg = vec4(texture2D(texture, gl_FragCoord.xy / resolution));
     gl_FragColor = clamp(bg + fg, 0., 1.);
 }
