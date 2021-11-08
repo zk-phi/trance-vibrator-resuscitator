4d76a1fbed27076b50424934bd1ec2142e0a6af1
zk-phi <zk-phi@users.noreply.github.com>
2021-11-08 21:56:13 +0900

    Refactoring

---
+++ AudioSource.js
@@ -0,0 +1,86 @@
+class AudioSource {
+  static fftSize = 1024;
+
+  static async getDevices () {
+    await navigator.mediaDevices.getUserMedia({ audio: true });
+    return await navigator.mediaDevices.enumerateDevices();
+  }
+
+  static audioMonitor (instance) {
+    if (instance.analyzer) {
+      instance.analyzer.getFloatTimeDomainData(instance.audioData);
+      const max = Math.max(...instance.audioData);
+      const min = Math.min(...instance.audioData);
+      const newValue = (max - min) / 2;
+      if (newValue > instance.value) {
+        instance.value = newValue;
+      } else {
+        instance.value = 0.9 * instance.value + 0.1 * newValue;
+      }
+      if (instance.chart) {
+        instance.chart.render(instance.audioData, instance.value, max !== min);
+      }
+    }
+  }
+
+  constructor (options) {
+    if (options.el) {
+      this.chart = new Chart({
+        el: options.el,
+        width: options.width || el.width,
+        height: options.height || el.height,
+        min: -1,
+        max: 1,
+      });
+      this.chart.initialize();
+    }
+    this.value = 0;
+    this.audioData = new Float32Array(AudioSource.fftSize);
+    this.analyzer = null;
+  }
+
+  async initWithStream (stream) {
+    const ctx = new AudioContext();
+    const source = new MediaStreamAudioSourceNode(ctx, {
+      mediaStream: stream,
+    });
+    const lpfNode = new BiquadFilterNode(ctx, {
+      type: "lowpass",
+      Q: 1,
+      frequency: 90,
+    });
+    const gainNode = new GainNode(ctx, {
+      gain: 3.5,
+    });
+    this.analyzer = new AnalyserNode(ctx, {
+      fftSize: AudioSource.fftSize,
+    });
+    const dest = new MediaStreamAudioDestinationNode(ctx);
+    source.connect(lpfNode).connect(gainNode).connect(this.analyzer).connect(dest);
+    setInterval(AudioSource.audioMonitor.bind(null, this), 30);
+  }
+
+  async initWithDevice (device) {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: { deviceId: device.deviceId },
+      video: false,
+    });
+    await this.initWithStream(stream);
+  }
+
+  async initWithDisplayMedia () {
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      video: true, /* required to be true by the API spec */
+      audio: true,
+    });
+    await this.initWithStream(stream);
+  }
+
+  getValue () {
+    return this.value;
+  }
+
+  getAudioData () {
+    return this.audioData;
+  }
+}
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const FFT_SIZE = 1024;
+const source = new AudioSource({
+  el: document.getElementById("source"),
+  width: 800,
+  height: 600,
+});;
 
 /* vo をあえて避けるなら -90 と 5000- くらいでいいかも */
 
 async function enumerateDevices () {
-  await navigator.mediaDevices.getUserMedia({ audio: true });
-  const devices = await navigator.mediaDevices.enumerateDevices();
+  const devices = await AudioSource.getDevices();
   const container = document.getElementById("audioDevice");
   container.innerHTML = "";
   devices.forEach(device => {
@@ -17,75 +20,20 @@ async function enumerateDevices () {
 }
 
 async function initCapture () {
-  const stream = await navigator.mediaDevices.getDisplayMedia({
-    video: true, /* required to be true by the API spec */
-    audio: true,
-  });
-  await streamInit(stream);
+  await source.initWithDisplayMedia();
   document.getElementById("audioDevice").innerHTML = `音声キャプチャに接続済み`;
 }
 
 async function initDevice (device) {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: { deviceId: device.deviceId },
-    video: false,
-  });
-  await streamInit(stream);
+  await source.initWithDevice(device);
   document.getElementById("audioDevice").innerHTML = `${device.label} に接続済み`;
-}
-
-let analyserNode;
-async function streamInit (stream) {
-  const ctx = new AudioContext();
-  const source = new MediaStreamAudioSourceNode(ctx, {
-    mediaStream: stream,
-  });
-  const lpfNode = new BiquadFilterNode(ctx, {
-    type: "lowpass",
-    Q: 1,
-    frequency: 90,
-  });
-  const gainNode = new GainNode(ctx, {
-    gain: 3.5,
-  });
-  analyserNode = new AnalyserNode(ctx, {
-    fftSize: FFT_SIZE,
-  });
-  const dest = new MediaStreamAudioDestinationNode(ctx);
-  source.connect(lpfNode).connect(gainNode).connect(analyserNode).connect(dest);
-  monitorAudio();
-  renderLevel();
-  sendVib();
-}
-
-let value = 0;
-const buf = new Float32Array(FFT_SIZE);
-const chart = new Chart({
-  el: document.getElementById("spectrum"),
-  width: 800,
-  height: 600,
-  min: -1,
-  max: 1,
-});
-chart.initialize();
-function monitorAudio () {
-  analyserNode.getFloatTimeDomainData(buf);
-  const max = Math.max(...buf);
-  const min = Math.min(...buf);
-  const newValue = (max - min) / 2;
-  if (newValue > value) {
-    value = newValue;
-  } else {
-    value = 0.9 * value + 0.1 * newValue;
-  }
-  chart.render(buf, value, max !== min);
-  setTimeout(monitorAudio, 30);
 }
 
 /* ---- */
 
 const valueEl = document.getElementById("value");
 function renderLevel () {
+  const value = source.getValue();
   valueEl.innerHTML = "|".repeat(value * 128) + " " + Math.floor(value * 255);
   requestAnimationFrame(renderLevel);
 }
@@ -96,9 +44,11 @@ async function connectVib () {
   const dev = new TranceVibrator();
   await dev.connect();
   devs.push(dev);
+  renderLevel();
+  sendVib();
 }
 
 function sendVib () {
-  devs.forEach(dev => dev.send(value, 0));
+  devs.forEach(dev => dev.send(source.getValue(), 0));
   setTimeout(sendVib, 60);
 }
